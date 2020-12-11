0cd5acdad8ae90a028de1754993098d2c4120ddc
Abdul waris sherzad <sherzadwaris52@gmail.com>
2020-12-11 23:27:58 +0200

    Correct version of getopt, which would help me to go for futher codding in it for doing my assignment

--- getopt.c
+++ getopt.c
@@ -2,43 +2,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-int
-main (int argc, char **argv)
+#include <time.h>
+#include <getopt.h>
+#include <string.h>
+
+int main(int argc, char **argv)
 {
-int aflag = 0;
-int bflag = 0;
-char *cvalue = NULL;
-int index;
-int c;
-opterr = 0;
-while ((c = getopt (argc, argv, "abc:")) != -1)
-switch (c)
-{
-case 'a':
-aflag = 1;
-break;
-case 'b':
-bflag = 1;
-break;
-case 'c':
-cvalue = optarg;
-break;
-case '?':
-if (optopt == 'c')
-fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-else if (isprint (optopt))
-fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-else
-fprintf (stderr,
-"Unknown option character `\\x%x'.\n",
-optopt);
-return 1;
-default:
-abort ();
-}
-printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-aflag, bflag, cvalue);
-for (index = optind; index < argc; index++)
-printf ("Non-option argument %s\n", argv[index]);
-return 0;
+	int option_val =0,counter =0, rand_num;
+	FILE *file;
+	time_t t;
+	char string[1000]= "";
+	while((option_val =getopt(argc,argv,"n:h:f:e")))
+	{
+		switch(option_val)
+		{
+			case'n':
+				if(optarg != NULL)
+				    counter = atoi(optarg);
+			    	for (int i=0; i <counter;++i)
+			    	printf("Hello world\n");
+				break;
+			case'h':
+				printf("Enter -h for help\n");
+				printf("Enter -e for a random even number between 0 and 99\n");
+				printf("Enter -n for 10 printed lines\n");
+				printf("Enter \"-f [filename]\" to print a file of your choice to stand output\n");
+				break;
+			case'f':
+				if((file = fopen(optarg,"r")))
+				{
+					while(fgets(string,sizeof(string),file))
+					{
+						printf("%s\n",string);
+					}
+				}
+				break;
+			case'e':
+				srand((unsigned) time(&t));
+				rand_num = rand()%100;
+				if(rand_num%2 !=0)
+				    rand_num +=1;
+				printf("%d\n",rand_num);
+				break;
+			default:
+				printf("You chose wrong");
+				return 0;
+		}
+	}
+	return 0;
 }
