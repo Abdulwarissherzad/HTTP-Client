22e1d450328bc96cab4e2842a1e8f57885e2b636
Abdul waris sherzad <sherzadwaris52@gmail.com>
2020-12-17 22:15:31 +0200

    I have completed all OPTIONS with getopt for http client which is for our assignment #4

--- getopt.c
+++ getopt.c
@@ -1,15 +1,18 @@
+/**********************************/
+/**-----http/1.1 client----------**/
+/**---Abdul waris sherzad--------**/
+/**********************************/
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
-#include <getopt.h>
 #include <string.h>
-
+/*Getopt Library*/
+#include <getopt.h>
 /*Socket Library*/
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 #define PORT 80
 
 int main(int argc, char **argv)
@@ -24,21 +27,17 @@ int main(int argc, char **argv)
     char buffer[1024] = {0};
     char hello[] = "";
 
-	while((option_val =getopt(argc,argv,"c:u:a:h:f:o:d:G"))!= -1)
+	while((option_val =getopt(argc,argv,"c:u:a:h:t:o:d:G"))!= -1)
 	{
 		switch(option_val)
 		{
 			case'c':{
-				    if (argc < 2 ) {
-				        printf("Please give hostname as arg\n");
-				        exit(0);
-				    }
 				    if(optarg != NULL)
 	                sprintf(hello,"GET / HTTP/1.1\r\nHost:%s\r\n\r\n",optarg);
                     }
 				break;
 			case'u':
-				sprintf(hello,"GET /~%s HTTP/1.1\r\nHost:%s\r\n\r\n",optarg,argv[2]);
+			//	sprintf(hello,"GET /~%s HTTP/1.1\r\nHost:%s\r\n\r\n",optarg,argv[2]);
 				break;
 			case 'a':
 		     	sprintf(hello,"GET / HTTP/1.1\r\nHost:%s\r\nUser-Agent:%s\r\n\r\n",argv[2],optarg);
@@ -47,24 +46,38 @@ int main(int argc, char **argv)
 				sprintf(hello,"GET / HTTP/1.1\r\nHost:%s\r\nX-First-Name:%s\r\n\r\n",argv[2],optarg);
 				break;
 			}
-			case'f':
-				if((file = fopen(optarg,"r"))) //optarg for reading of file
+			case't':
+				if((file = fopen(optarg,"r"))) //optarg for reading name of file
 				{
-					while(fgets(string,sizeof(string),file))
-					{
-						printf("%s\n",string);
-					}
+                while(fgets(string,sizeof(string),file))
+                {   
+				    // printf("%s\n",string);
+					sprintf(hello,"POST /~devrim/test.php HTTP/1.1\r\nHost:db.ciu.edu.tr \r\n\r\n","%s\n \n\r",string);
+	            }
 				}
 				fclose(file);
 				break;
+
+			case'?':
+				if (optopt == 'u'){
+				 sprintf(hello,"GET /~%s HTTP/1.1\r\nHost:%s\r\n\r\n",optarg,argv[2]);
+            	}else
+				break;
+			case'd':
+				if(optarg != NULL)
+				 sprintf(hello, "POST /~devrim/test.php HTTP/1.1\r\nHost:db.ciu.edu.tr\r\n\r\n%s&%s\r\n",optarg,optarg);
+				break;
+			case'G':
+				if(optarg != NULL)
+				sprintf(hello,"GET /~devrim/test.php?%s&%s HTTP/1.1\r\nHost:db.ciu.edu.tr\r\n\r\n",optarg,optarg);
+				break;
 			case 'o':{
-				if((fileout = fopen(optarg,"w"))){
+				if(fileout = fopen(optarg,"w")){
                    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 				    {
 				        printf("\n Socket creation error \n");
 				        return -1;
 				    }
-
 				    serv_addr.sin_family = AF_INET;
 				    serv_addr.sin_port = htons(PORT);
 				    inet_aton(argv[1],&serv_addr.sin_addr);
@@ -77,31 +90,18 @@ int main(int argc, char **argv)
 				    send(sock , hello , strlen(hello) , 0 );
 				    valread = read( sock , buffer, 1024);
 					fprintf(fileout,"%s\n",buffer);
-				}
-				fclose(fileout);
-				printf("\nResponse file has been written to <file:%s> instead of stdout.\n\n",optarg);
-				return 0;
+			    	}
+				    fclose(fileout);
+			    	printf("\nResponse file has been written to <file:%s> instead of stdout.\n\n",optarg);
+			    	return 0;
 				break;
-			}
-			case'?':
-
-				if (optopt == 'u'){
-
-            	}else
-				break;
-			case'd':
-				if(optarg != NULL)
-				 sprintf(hello, "POST /~devrim/test.php HTTP/1.1\r\nHost:db.ciu.edu.tr\r\n\r\n%s&%s\r\n",optarg,optarg);
-				break;
-			case'G':
-				if(optarg != NULL)
-				sprintf(hello,"GET /~devrim/test.php?%s&%s HTTP/1.1\r\nHost:db.ciu.edu.tr\r\n\r\n",optarg,optarg);
-				break;
-			default:{
+		    	}
+			default:
+			{
 				return 0;
 			}
 		}
-   }
+    }
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 	    printf("\n Socket creation error \n");
@@ -113,14 +113,13 @@ int main(int argc, char **argv)
 
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-	    printf("\nConnection Failed \n");
+		printf("\nConnection Failed \n");
 		return -1;
 	}
-	send(sock , hello , strlen(hello) , 0 );
+ 	send(sock , hello , strlen(hello) , 0 );
     printf("Message sent \n%s",hello);
-	valread = read( sock , buffer, 1024);
-	printf("%s\n",buffer );
-				    
+   	valread = read( sock , buffer, 1024);
+ 	printf("%s\n",buffer );
 	return 0;
 }
 
